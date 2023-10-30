f427fa9128d5360c7f5e362e450854fc6beedbcb
hussein.kanso <kanso275@gmail.com>
2023-10-30 20:50:47 +0200

    ..

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,34 @@ app.listen(port,()=>{
 const __dirname = path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
+// if(process.env.NODE_ENV==='production'){
+//     // set static folder
+//     app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+//     // any route that is not api route
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
+//     })
+// }else 
+// {
+//     app.get('/',(req,res)=>{
+//         res.send('API is running')
+//     })
+// }
+
+
 if(process.env.NODE_ENV==='production'){
     // set static folder
     app.use(express.static(path.join(__dirname,'/frontend/dist')))
+}
 
-    // any route that is not api route
+app.use('/api/products',productRoutes)
+app.use('/api/users',userRoute)
+app.use('/api/orders',orderRoutes)
+app.use('/api/upload',uploadRoutes)
+app.use(notFound,errorHandler)
+
+if(process.env.NODE_ENV==='production'){
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
     })
@@ -42,9 +65,3 @@ if(process.env.NODE_ENV==='production'){
         res.send('API is running')
     })
 }
-
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoute)
-app.use('/api/orders',orderRoutes)
-app.use('/api/upload',uploadRoutes)
-app.use(notFound,errorHandler)
