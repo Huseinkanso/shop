78a2d28e41ad8891bf516b5947dbec1b36b54f51
hussein.kanso <kanso275@gmail.com>
2023-10-31 17:08:13 +0200

    ..

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+dotenv.config();
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -7,7 +8,6 @@ const generateToken = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    maxAge: 30 * 24 * 60 * 60 * 1000,
     // for https
     secure: process.env.NODE_ENV === "production",
     // like same origin policy
